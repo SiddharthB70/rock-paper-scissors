b3425eadd3cc4632892c8b7fd1f3d3036e81df68
SiddharthB70 <siddharth7002b@gmail.com>
2021-12-02 19:53:49 +0530

    Created 'decideResult' function, defined 'playerSelection' for input out of give scope

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function userInput(){
+    let playerSelection;
+    do{
+        playerSelection = prompt("Rock, Paper or Scissor").toUpperCase();
+        console.log(playerSelection);
+    }while(playerSelection!="ROCK" && playerSelection!="PAPER" && playerSelection!="SCISSOR");
+    return playerSelection;
+}
+
 function randInt(n){
     return Math.floor(Math.random()*n);
 }
@@ -11,13 +20,38 @@ function computerPlay(){
     }
 }
 
+function decideResult(playerSelection,computerSelection){
+    switch(computerSelection){
+        case "Rock": 
+            if(playerSelection=="ROCK")
+                return "It's a tie!";
+            else if(playerSelection=="PAPER")
+                return "You win! Paper beats Rock";
+            else if(playerSelection=="SCISSOR")
+                return "You lose! Rock beats Scissor";
+        case "Paper": 
+            if(playerSelection=="PAPER")
+                return "It's a tie!";
+            else if(playerSelection=="SCISSOR")
+                return "You win! Scissor beats Paper";
+            else if(playerSelection=="ROCK")
+                return "You lose! Paper beats Rock";
+        case "Scissor": 
+            if(playerSelection=="SCISSOR")
+                return "It's a tie!";
+            else if(playerSelection=="ROCK")
+                return "You win! Rock beats Scissor";
+            else if(playerSelection=="PAPER")
+                return "You lose! Scissor beats Paper";
+    }
+}
+
 function game(){
     for(let i=0; i<5; i++)
         {
-            let playerSelection = prompt("Rock, Paper or Scissors");
+            let playerSelection = userInput();
             let computerSelection = computerPlay();
-            console.log(computerSelection);
-            decideResult(playerSelection,computerSelection);
+            alert(decideResult(playerSelection,computerSelection));
         }
 }
 
