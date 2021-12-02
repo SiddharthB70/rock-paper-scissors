4ce7a47d67f1420e24b564432126c2aa44715d72
SiddharthB70 <siddharth7002b@gmail.com>
2021-12-02 19:10:35 +0530

    Created 'computerSelection' function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+function randInt(n){
+    return Math.floor(Math.random()*n);
+}
+
+function computerPlay(){
+    let compSelection = randInt(3);
+    switch(compSelection){
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissor";
+    }
+}
+
 function game(){
     for(let i=0; i<5; i++)
         {
             let playerSelection = prompt("Rock, Paper or Scissors");
             let computerSelection = computerPlay();
+            console.log(computerSelection);
             decideResult(playerSelection,computerSelection);
         }
 }
+
+game();
