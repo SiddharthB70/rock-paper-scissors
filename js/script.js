4760cfe321cbaea07d874bc9dde42986a7378960
SiddharthB70 <siddharth7002b@gmail.com>
2022-01-25 18:14:56 +0530

    Added Cards Hover Functionality

--- js/script.js
+++ js/script.js
@@ -1,58 +1,23 @@
-// function userInput(){
-//     let playerSelection;
-//     do{
-//         playerSelection = prompt("Rock, Paper or Scissor").toUpperCase();
-//         console.log(playerSelection);
-//     }while(playerSelection!="ROCK" && playerSelection!="PAPER" && playerSelection!="SCISSOR");
-//     return playerSelection;
-// }
+function hoverCards(){
+    const playerCards = document.querySelector(".cards");
+    playerCards.addEventListener("mouseenter",splitCards);
+    playerCards.addEventListener("mouseleave",joinCards);
+}
 
-// function randInt(n){
-//     return Math.floor(Math.random()*n);
-// }
+function splitCards(){
+    const cards = Array.from(this.children);
+    this.classList.add("cards-hover");
+    cards.forEach(function(card){
+        card.classList.add("hover");
+    })
+}
 
-// function computerPlay(){
-//     let compSelection = randInt(3);
-//     switch(compSelection){
-//         case 0: return "Rock";
-//         case 1: return "Paper";
-//         case 2: return "Scissor";
-//     }
-// }
+function joinCards(){
+    const cards = Array.from(this.children);
+    this.classList.remove("cards-hover");
+    cards.forEach(function(card){
+        card.classList.remove("hover");
+    })
+}
 
-// function decideResult(playerSelection,computerSelection){
-//     switch(computerSelection){
-//         case "Rock": 
-//             if(playerSelection=="ROCK")
-//                 return "It's a tie!";
-//             else if(playerSelection=="PAPER")
-//                 return "You win! Paper beats Rock";
-//             else if(playerSelection=="SCISSOR")
-//                 return "You lose! Rock beats Scissor";
-//         case "Paper": 
-//             if(playerSelection=="PAPER")
-//                 return "It's a tie!";
-//             else if(playerSelection=="SCISSOR")
-//                 return "You win! Scissor beats Paper";
-//             else if(playerSelection=="ROCK")
-//                 return "You lose! Paper beats Rock";
-//         case "Scissor": 
-//             if(playerSelection=="SCISSOR")
-//                 return "It's a tie!";
-//             else if(playerSelection=="ROCK")
-//                 return "You win! Rock beats Scissor";
-//             else if(playerSelection=="PAPER")
-//                 return "You lose! Scissor beats Paper";
-//     }
-// }
-
-// function game(){
-//     for(let i=0; i<5; i++)
-//         {
-//             let playerSelection = userInput();
-//             let computerSelection = computerPlay();
-//             alert(decideResult(playerSelection,computerSelection));
-//         }
-// }
-
-// game();
+hoverCards();
