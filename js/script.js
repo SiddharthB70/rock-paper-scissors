bfabdbdcb3d030f0f23849ab9d9d96006b74fc0a
SiddharthB70 <siddharth7002b@gmail.com>
2022-01-26 22:06:39 +0530

    Added Card Click

--- js/script.js
+++ js/script.js
@@ -1,23 +1,57 @@
-function hoverCards(){
+function hover(){
     const playerCards = document.querySelector(".cards");
     playerCards.addEventListener("mouseenter",splitCards);
-    playerCards.addEventListener("mouseleave",joinCards);
+    playerCards.addEventListener("mouseleave",joinCards)
 }
 
 function splitCards(){
-    const cards = Array.from(this.children);
-    this.classList.add("cards-hover");
-    cards.forEach(function(card){
-        card.classList.add("hover");
+    const cardContainers = document.querySelectorAll(".cards");
+        cardContainers.forEach(function(container){
+            container.classList.add("cards-hover");
+            const cards = Array.from(container.children);
+            cards.forEach(function(card){
+                card.classList.add("hover");
+                if(container.classList.contains("player")){
+                    card.addEventListener("click",getPlayerCard);
+                    card.addEventListener("mouseenter",scaleCard);
+                    card.addEventListener("mouseleave",returnScale);
+                }
+            })
     })
 }
 
 function joinCards(){
-    const cards = Array.from(this.children);
-    this.classList.remove("cards-hover");
-    cards.forEach(function(card){
-        card.classList.remove("hover");
+    const cardContainers = document.querySelectorAll(".cards");
+    cardContainers.forEach(function(container){
+        container.classList.remove("cards-hover");
+        const cards = Array.from(container.children);
+        cards.forEach(function(card){
+            card.classList.remove("hover");
+        }) 
     })
 }
 
-hoverCards();
+function getPlayerCard(e){
+    e.target.id = "player-card";
+    e.stopImmediatePropagation();
+    const playerCards = document.querySelector(".cards");
+    playerCards.removeEventListener("mouseenter",splitCards);
+    playerCards.removeEventListener("mouseleave",joinCards);
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(function(card){
+        card.removeEventListener("click",getPlayerCard);
+        card.removeEventListener("mouseenter",scaleCard);
+        card.removeEventListener("mouseleave",returnScale);
+        card.removeAttribute("style");
+    })
+}
+
+function scaleCard(){
+    this.setAttribute("style","transform: scale(1.1)")
+}
+
+function returnScale(){
+    this.removeAttribute("style");
+}
+
+hover();
